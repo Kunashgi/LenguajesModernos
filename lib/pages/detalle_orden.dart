c33e1877dd01a4d362fb531e21e88e7fe254b10f
Payo13 <claudio.basket13@gmail.com>
2022-06-07 22:32:20 -0400

    detalle orden colores y asociacion texto

--- lib/pages/detalle_orden.dart
+++ lib/pages/detalle_orden.dart
@@ -15,17 +15,45 @@ class Detalle_orden extends StatelessWidget {
   }
 
   Card miCard() {
+    var titulo = 'Creacion de Mockups';
+    var estado = 'Activo';
+    var inicio = '08-04-2022';
+    var finalizacion = '30-05-2022';
+    var colaborador = 'Mauricio Arrieta';
+    var responsable = 'Matias Pérez';
+    var proyecto = 'Creación App';
+    var direccion = 'Providencia 425';
+    var telefono = '988944902';
+
+    //se crea el texto a utilizar en subtitle con los datos desde backend
+    var datofinal = (titulo +
+        "\nEstado: " +
+        estado +
+        "\nInicio: " +
+        inicio +
+        "\nFinalización: " +
+        finalizacion +
+        "\nColaborador: " +
+        colaborador +
+        "\nResponsable: " +
+        responsable +
+        "\nProyecto: " +
+        proyecto +
+        "\nDirección: " +
+        direccion +
+        "\nTeléfono: " +
+        telefono);
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       margin: EdgeInsets.all(15),
+      color: Color.fromARGB(255, 15, 190, 202),
       elevation: 10,
       child: Column(
         children: <Widget>[
           ListTile(
             contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
             title: Text('CM001'),
-            subtitle: Text(
-                'Creación de Mockups \nFecha:20-05-2022 \nSin fecha de término \nColaborador:Mauricio Arrieta \nResponsable:Matias Pérez \nProyecto:Creación App \nDireccion:Pedro de Valvidia 425 \nNumero: 987654321'),
+            subtitle: Text(datofinal),
             leading: Icon(Icons.info),
           ),
         ],
